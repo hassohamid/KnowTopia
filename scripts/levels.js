7f167426f21027afde60e71210f99ce445f7441e
hassohamid <hassohamid@hotmail.com>
2024-10-06 14:49:32 +0200

    HTML structure, renamed CSS accordingly

---
+++ scripts/levels.js
@@ -0,0 +1,11 @@
+function beginner() {
+  window.location = "/pages/beginner.html";
+}
+
+function pro() {
+  window.location = "/pages/pro.html";
+}
+
+function elite() {
+  window.location = "/pages/elite.html";
+}
